9fc4fe18c6f72c51d138ddc24b09453e83b83117
ocean1229 <ocean1229@naver.com>
2021-03-30 15:18:37 +0900

    js 기초 02

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ console.log(name);
 // 세미콜론?
 // 문자도 가능
 
-var personArray = [10, 20, 30];
-console.log(personArray);
-console.log(personArray[0]);
+var numberArray = [10, 20, 30];
+console.log(numberArray);
+console.log(numberArray[0]);
 
 var personDict = {"name" : "두현", "age" : 19};
 // 객체 -> key : value
@@ -23,4 +23,33 @@ if(personDict['age']>19){
     console.log('Here is your beer!')
 }else{
     console.log('Get out!!')
+};
+
+// 반복문
+var personArray = [
+    {"name" : "두현", "age" : 19},
+    {"name" : "승연", "age" : 22},
+];
+for(var i=0; i<personArray.length; i++){
+    console.log(personArray[i])
 }
+
+//함수
+function isValidAge(person){
+    if(person["age"]>19){
+        return true
+    }else{
+        return false
+    };
+}
+
+for(var i=0; i<personArray.length; i++){
+    console.log(personArray[i]['age'])
+    if(isValidAge(personArray[i])){
+        console.log('Here is your beer!');
+    }else{
+        console.log('Get out!')
+    }
+}
+
+// 함수
