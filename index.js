9dcc714598749787426d5378753f2451056f1ee4
ocean1229 <ocean1229@naver.com>
2021-03-31 11:04:15 +0900

    var let const

--- index.js
+++ index.js
@@ -53,3 +53,20 @@ for(var i=0; i<personArray.length; i++){
     }
 }
 
+// let 과 var의 차이점
+// var로 선언하였을때 
+// 바깥에 선언한 var의 경우 안에서 var의 변수를 바꾸면 
+// 바깥에 선언한 var에도 영향을 끼치지만
+
+// let로 선언하였을때
+// 바깥에 선언한 let의 경우 안에서 let의 변수를 바꾸면
+// 바깥에 선언한 let에 영향을 끼치지 않는다.
+// 새로 생성된 안쪽 부분을 새로운 (inner)scope이라고 부른다
+
+// const는 여기에 변수 값을 지정하고 나면 끝날때까지 절대 변하지 않는 변수를 의미합니다.
+// const상수라고 부른다
+
+// var,let,const비교   var         let         const비교
+// 재선언              O           X           X
+// 재할당              O           O           X
+
